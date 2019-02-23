8c2c40e6d4f6f43f288c81bb1fec4be10b5915f1
aravind-sundaresan <sundaresan.aravind2@gmail.com>
2019-02-23 01:55:14 -0700

    Implemeting Task 1

---
+++ javaminibase/src/global/intervaltype.java
@@ -0,0 +1,14 @@
+package global;
+
+public class intervaltype {
+    int s;
+    int e;
+    int l;
+
+    public void assign(int a, int b, int c) {
+        this.s = a;
+        this.e = b;
+        this.l = c;
+    }
+}
+
